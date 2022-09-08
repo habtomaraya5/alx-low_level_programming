5bc9292d5ad63b074062f6d158f83e5fc6078843
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-08 14:07:45 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#includ <stdio.h>
+/**
+ *main -prints a text
+ *
+ *Return: Always 0(Success)
+ */
+int main(void)
+{
+     printf("with proper program, but the outcome is apiece of art,\n");
+     return(0);
+}
