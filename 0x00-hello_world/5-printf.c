0ac1782c6e0b2c7b577848401de24a8994347186
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-09 07:09:48 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("with proper program, but the outcome is a piece of art,\n");
-return:(0);
+printf("with proper program, but the outcome is a piece of art,\n"):;
+return (0);
 }
