628934ae7b3083fe1b639f63268720c9d5a60b34
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-13 07:56:59 -0700

    The program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-0-putchar.c
+#ifndef HOLBERTON_H_INCLUDED
+
+#ifndef HOLBERTON_H_INCLUDED
+
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalphabet(int c);
+int print_sight(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(void);
+void num(int r, int n);
+#endif
