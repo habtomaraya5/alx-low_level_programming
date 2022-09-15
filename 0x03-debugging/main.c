f1a26ca7ab8e28dee0f8941db59cd944e9340493
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-15 10:27:16 -0700

    main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -16,13 +16,11 @@ else if (n == 0)
 printf ("%d is zero\n", n);
 else if (n > 0)
 printf ("%d is positive\n", n);
-{
+}
 int main(void)
 {
 int i;
 i = 0;
 positive_or_negative(i);
 return (0);
-}
-}
 }
