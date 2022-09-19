29cb0d2682d1cb260f9e4feb04901040c1410f09
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-19 08:58:07 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,7 +5,9 @@
  *
  * Return: Always 0.
  */
-void rev_string(char *s)
+void(main)
+{
+rev_string(char *s)
 {
 char rev = s[0];
 int counter = 0;
@@ -18,5 +20,6 @@ counter--;
 rev = s[i];
 s[i] = s[counter];
 s[counter] = rev;
+}
 }
 }
