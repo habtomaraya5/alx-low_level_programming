3824e16fce3a550d0838294ada0bc7f42d53f346
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-20 06:20:54 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,9 +7,16 @@
  */
 void  puts_half(char *str)
 {
-int len, i;
-len = strlen(str);
-for (i = 0; i < len; i++)
-_putchar(str[i]);
+int i, l;
+for (i = 0; str[i] != '\0'; i++)
+if(i % 2 == 0){
+for (l = i / 2; str[l] != ''\0; l++)
+_putchar(str[l]);
+}
+else
+{
+for (l = ((i - 1) / 2) + 1; str[l] != '\0'; l++)
+_putchar(str[l]);
+}
 _putchar('\n');
 }
