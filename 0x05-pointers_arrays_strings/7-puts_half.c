47281b0d8ad3b06ceb727c39e40270e4dd085ab7
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-09-20 06:31:38 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,23 @@
 #includ "main.h"
+#include <string.h>
 
 /**
- *puts_half(char *str) -> puts function
  *
+ *puts_half(char *str) -> puts function
  * @str: str parametr
  */
 void  puts_half(char *str)
 {
-int i, l;
-for (i = 0; str[i] != '\0'; i++)
+int l, n;
+for (l = 0; str[l] != '\0'; l++)
 if(i % 2 == 0){
-for (l = i / 2; str[l] != ''\0; l++)
-_putchar(str[l]);
+for (n = l / 2; str[l] != '\0'; l++)
+_putchar(str[n]);
 }
 else
 {
-for (l = ((i - 1) / 2) + 1; str[l] != '\0'; l++)
-_putchar(str[l]);
+for (n = ((l - 1) / 2) + 1; str[n] != '\0'; n++)
+_putchar(str[n]);
 }
 _putchar('\n');
 }
