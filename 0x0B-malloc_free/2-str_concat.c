071a1655bc5c475107f47fa797289eb7df760458
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-10-22 16:10:53 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,53 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* str_concat - concatnets two strings for those destinations
-* @s1: destination
-* @s2: source
-* Return: destination
-*/
+  *_strlen - counts and returns string length
+  * @s: that's the string
+  *
+  * Return: the length
+  */
+int _strlen(char *s)
+{
+int counter = 0;
+if (!*s)
+return (0);
+while (*s)
+{
+counter++;
+s++;
+}
+return (counter);
+}
+/**
+ * str_concat - concatenates two strings
+ * @s1: one string
+ * @s2: the other
+ * Return: pointer to cat string
+ */
 char *str_concat(char *s1, char *s2)
 {
-int i = 0, j = 0, len1 = 0, len2 = 0, totlen = 0;
-char *str;
-while (s1[i++])
-len1++;
-i = 0;
-while (s2[i++])
-len2++;
-totlen = len1 + len2 + 1;
-str = malloc(sizeof(char) * totlen);
-for (i = 0; i < len1; i++)
+char *new;
+unsigned int i;
+unsigned int j;
+int total = 0;
+if (!s1)
+s1 = "";
+if (!s2)
+s2 = "";
+total += _strlen(s1) + _strlen(s2);
+new = malloc((total * sizeof(char)) + 1);
+if (new == NULL)
 {
-if ((str + i) == NULL)
-{
-printf("failed to allocate memory\n");
 return (NULL);
 }
-*(str + i) = *(s1 + i);
-}
-for (i = len1, j = 0; i < totlen; i++, j++)
+for (i = 0; s1[i]; i++)
 {
-if ((str + i) == NULL)
+new[i] = s1[i];
+}
+for (j = 0; s2[j]; j++, i++)
 {
-printf("failed to allocate memory\n");
-return (NULL);
+new[i] = s2[j];
 }
-*(str + i) = *(s2 + j);
-}
-return (str);
+new[i] = '\0';
+return (new);
 }
