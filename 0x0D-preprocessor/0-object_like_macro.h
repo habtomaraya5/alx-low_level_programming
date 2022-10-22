a515fb8eafc98f72205f9661a35e81790199d3e2
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-10-22 14:16:26 +0300

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * Addis Ababa, ETHIOPIA
+ * Auth:Habtom Araya
+ */
+
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * Addis Ababa, ETHIOPIA
+ * Auth: HabtomAraya
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+#endif
